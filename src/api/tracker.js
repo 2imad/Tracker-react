c9b756bf8da9f52cdd3023c754a7d124ca22f1b3
2imad <imadyoussoufi@gmail.com>
2019-10-20 00:21:25 +0200

    add styling

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-   baseURL: "http://724b8273.ngrok.io"
+   baseURL: "http://ba80d163.ngrok.io"
 
 });
 
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,22 +1,9 @@
 import React, { useContext } from "react";
 import { View, StyleSheet, TouchableOpacity, FlatList } from "react-native";
 import { NavigationEvents } from 'react-navigation';
-import { ListItem } from 'react-native-elements';
+import { ListItem, Card, Image } from 'react-native-elements';
 import { Context as TrackContext } from '../context/TrackContext';
-
-const list = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-
-]
+import hikeAsset from '../../assets/images/hike.png'
 
 
 const TrackListScreen = ({ navigation }) => {
@@ -24,42 +11,50 @@ const TrackListScreen = ({ navigation }) => {
   return (
     <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      {
-        list.map((l, i) => (
-          <ListItem
-            style={styles.content}
-            key={i}
-            leftAvatar={{ source: { uri: l.avatar_url } }}
-            title={l.name}
-            subtitle={l.subtitle}
-            bottomDivider
-            chevron
-          />
-        ))
-      }
+      <Card
+        containerStyle={styles.container}
+        title='TRACKS'
+        titleStyle={styles.content}
+        imageStyle={styles.img}
+        image={require('../../assets/images/hike.jpeg')}>
 
-
-      {/* <FlatList
-        data={state}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => {
-          return <TouchableOpacity onPress={() => navigation.navigate('TrackDetail', { _id: item._id })} >
-            <ListItem
-              key={i}
-              leftAvatar={{ source: { uri: l.avatar_url } }}
-              title={l.name}
-              subtitle={l.subtitle}
-              bottomDivider
-            />
-          </TouchableOpacity>
-        }}
-      /> */}
+        <FlatList
+          data={state}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => {
+            return <TouchableOpacity onPress={() => navigation.navigate('TrackDetail', { _id: item._id })} >
+              <ListItem
+                leftAvatar={{ source: hikeAsset }}
+                title={item.name}
+                titleStyle={styles.content}
+                subtitle={item.name}
+                bottomDivider
+                chevron
+              />
+            </TouchableOpacity>
+          }}
+        />
+      </Card>
     </>
   );
 };
+
+TrackListScreen.navigationOpltions = {
+
+}
+
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  img: {
+    aspectRatio: 1
+  },
   content: {
-    fontFamily: 'Montserrat'
+    fontFamily: 'Montserrat',
+    fontSize: 20,
+    fontWeight: '700'
   }
 });
 export default TrackListScreen;
